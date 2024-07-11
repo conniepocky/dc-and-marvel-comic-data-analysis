d5bc24866d13ab66a800a8b8d06239aa931f27ec
Connie <c.amer@icloud.com>
2024-07-11 16:56:25 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dcdf = pd.read_csv('data/dc-wikia-data.csv')
+marveldf = pd.read_csv('data/marvel-wikia-data.csv')
+
+dcdf['Publisher'] = 'DC'
+marveldf['Publisher'] = 'Marvel'
+
+df = pd.concat([dcdf, marveldf]) 
+
+#order by apperarances
+
+df = df.sort_values(by='APPEARANCES', ascending=False)
+
+#change names, clean up and remove brackets
+
+df['name'] = df['name'].str.replace('\([^()]*\)', '', regex=True).str.title()
+
+df = df[df['ID'] != 'Identity Unknown']
+
+print(df.head(10))
+
+# distribution of first appearance 
+
+dcFirst = plt.figure()
+
+plt.title("DC Distribution of first appearance by year")
+
+sns.histplot(dcdf["YEAR"], bins=20, kde=True, color='violet')
+
+marvelFirst = plt.figure()
+
+plt.title("Marvel Distribution of first appearance by year")
+
+sns.histplot(marveldf["Year"], bins=20, kde=True, color='orange')
+
+#plot the top 10 characters by appearances
+
+top10 = plt.figure()
+
+colors = ['red', 'blue']
+
+sns.barplot(x='APPEARANCES', y='name', data=df.head(10), hue='Publisher', palette=colors)
+x = df.head(10)['APPEARANCES']
+y = df.head(10)['name']
+
+plt.ylabel('Name')
+plt.xlabel("Appearances")
+
+for index, value in enumerate(x):
+    plt.text(value, index,
+             str(value))
+    
+
+plt.tight_layout()
+plt.show()
+
